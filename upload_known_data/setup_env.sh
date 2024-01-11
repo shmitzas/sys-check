a773151d5922cbe9c2eb31bb93a380d5993d1fac
shmitzas <46751666+shmitzas@users.noreply.github.com>
2024-01-11 20:53:55 +0200

    minor tweaks

--- upload_known_data/setup_env.sh
+++ upload_known_data/setup_env.sh
@@ -3,9 +3,9 @@
 sys_check_repo_location="$1"
 
 #setup environment configuration files
-mkdir ~/.sys-check/
-mkdir ~/.sys-check/.env/
-cp "${sys_check_repo_location}/upload_known_data/.env.example" ~/.sys-check/.env/upload_data.env
+mkdir /tmp/sys-check/
+mkdir /tmp/sys-check/.env/
+cp "${sys_check_repo_location}/upload_known_data/.env.example" /tmp/sys-check/.env/upload_data.env
 
 #setup golang
 sudo apt install -y golang-go
